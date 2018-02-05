8dc31ed0cd52328d802f61ceb02c4380b2c2f83a
tpoterba <tpoterba@broadinstitute.org>
2018-02-05 18:20:08 -0500

    Fix GT

--- src/test/scala/is/hail/io/HardCallsSuite.scala
+++ src/test/scala/is/hail/io/HardCallsSuite.scala
@@ -9,7 +9,7 @@ import org.testng.annotations.Test
 class HardCallsSuite extends SparkSuite {
   @Test def test() {
     val p = forAll(MatrixTable.gen(hc, VSMSubgen.random)) { vds =>
-      val hard = vds.annotateGenotypesExpr("g = {GT: g.GT}")
+      val hard = vds.selectEntries("GT")
       assert(hard.queryGenotypes("gs.map(g => g.GT).counter()") ==
         vds.queryGenotypes("gs.map(g => g.GT).counter()"))
 
